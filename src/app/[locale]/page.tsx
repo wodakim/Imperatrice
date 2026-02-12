12728116f5e2cab55c93731794420b874e2dd12f
wodakim <117268132+wodakim@users.noreply.github.com>
2026-02-12 21:20:29 +0100

    Update page.tsx

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,13 @@
-export default function Page() {
-  return (
-    <div>
-      <h1>Ça fonctionne !</h1>
-    </div>
-  );
+import { redirect } from 'next/navigation';
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}) {
+  // On attend que les paramètres soient chargés (spécifique à Next.js 15/16)
+  const { locale } = await params;
+
+  // On redirige l'utilisateur vers le dashboard tout en gardant sa langue
+  redirect(`/${locale}/dashboard`);
 }
